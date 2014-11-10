964dca5b6a23d2be64d1e2f545e134e9be2ab57c
tzhang01 <tinazhang1197@gmail.com>
2014-11-09 23:24:33 -0500

    Create driver.java

---
+++ 01SuperArray/driver.java
@@ -0,0 +1,29 @@
+public class driver{
+    public static void main(String[]args){
+	Integer x = new Integer(0);
+	superArray L = new superArray();
+	L.add(x);
+       	L.add(new Integer(1));
+	L.add(new Integer(2));
+	L.add(new Integer(3));
+	L.add(new Integer(4));
+	L.add(new Integer(5));
+	System.out.println(L);
+	System.out.println(L.size());
+	L.resize(15);
+	System.out.println(L);
+	System.out.println(L.size());
+	//L.clear();
+	System.out.println(L);
+	System.out.println(L.get(0));
+	System.out.println(L.get(1));
+	L.set(5, x);
+	System.out.println(L);
+	System.out.println(L.get(3)); 
+	System.out.println(L.get(0));
+	System.out.println(L.get(15));
+	System.out.println(L.get(30));
+	L.add(3, 7);
+	System.out.println(L);			   
+    }
+}
