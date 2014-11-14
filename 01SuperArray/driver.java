45fdfccce3055b3a1afe9cf9a728d4636f5577a4
tzhang01 <tinazhang1197@gmail.com>
2014-11-13 21:29:22 -0500

    updated driver

--- 01SuperArray/driver.java
+++ 01SuperArray/driver.java
@@ -1,31 +1,23 @@
 public class driver{
-    public static void main(String[]args){
-	Integer x = new Integer(0);
+	public static void main(String[]args){
 	superArray L = new superArray();
-	L.add(x);
-       	L.add(new Integer(1));
-	L.add(new Integer(2));
-	L.add(new Integer(3));
-	L.add(new Integer(4));
-	L.add(new Integer(5));
-	System.out.println(L);
+	for(int i=0; i < L.size(); i++){
+		L.add(i);
+	}
+	System.out.println(L);		
+	L.add("Hi");
 	System.out.println(L.size());
-	L.resize(15);
-	System.out.println(L);
+	L.add(1);
 	System.out.println(L.size());
-	//L.clear();
-	System.out.println(L);
-	System.out.println(L.get(0));
-	System.out.println(L.get(1));
-	L.set(5, x);
-	System.out.println(L);
-	System.out.println(L.get(3)); 
-	System.out.println(L.get(0));
-	System.out.println(L.get(15));
-	System.out.println(L.get(30));
-	//L.add(3, 7);
-	//System.out.println(L);
-	System.out.println(L.remove(3));
-	System.out.println(L.remove(15));			   
-    }
+	L.add(2);
+	System.out.println(L.size());
+	L.add("sup");
+	System.out.println(L.size());
+	System.out.println(L.toString());
+	L.add(9);
+	System.out.println(L.size());
+	System.out.println(L.toString());
+	}
 }
+	
+	
