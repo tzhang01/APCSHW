209b99c01cb8ac4f0f7f5cc64cc668ffff9ffc60
tzhang01 <tinazhang1197@gmail.com>
2014-11-20 00:22:07 -0500

    fixed clear method and added toString method

--- 03WordSearch/WordGrid.java
+++ 03WordSearch/WordGrid.java
@@ -1,9 +1,24 @@
+import java.util.*;
+
 public class WordGrid{
     private char[][]data;
+	
+	/**Initialize the grid to the size specified and fill all of the positions
+     *with spaces.
+     *@param row is the starting height of the WordGrid
+     *@param col is the starting width of the WordGrid
+     */
     public WordGrid(int rows, int cols){
-		char[][]data = new char [rows][cols];
+		data = new char [rows][cols];
 		clear();
     }
+	
+	//default constructor initializes a 5x5 grid
+	public WordGrid(){
+		this(5,5);
+	}
+	
+	/**Set all values in the WordGrid to spaces ' '*/
 	public void clear(){
 		for(int i=0; i<data.length;i++){
 			for(int j=0; j<data[i].length; j++){
@@ -12,12 +27,24 @@ public class WordGrid{
 		}
 	}
 
+	/**The proper formatting for a WordGrid is created in the toString.
+     *@return a String with each character separated by spaces, and each row
+     *separated by newlines.
+     */
+	public String toString(){
+		String result = "";
+		for(int i=0;i<data.length;i++){
+			for(int j=0;j<data[i].length;j++){
+				result += data[i][j] + " ";
+			}
+			result += "\n";
+		}
+		return result;
+	}
 		
-
+	//testing
 	public static void main(String[]args){
-		WordGrid grid = new WordGrid(5,5);
-		System.out.println(grid);
-	} 
-
-
+		WordGrid data = new WordGrid(6,6);
+		System.out.println("Should print out a grid of spaces" + data);
+	}
 }
