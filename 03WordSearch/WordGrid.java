79ce3c45d40267fe050a07e5718dc54572e69adc
tzhang01 <tinazhang1197@gmail.com>
2014-11-20 00:02:12 -0500

    added clear method, but the adding the spaces isnt working?

--- 03WordSearch/WordGrid.java
+++ 03WordSearch/WordGrid.java
@@ -1,6 +1,23 @@
 public class WordGrid{
     private char[][]data;
     public WordGrid(int rows, int cols){
-	char[][]data = new char [rows][cols];
+		char[][]data = new char [rows][cols];
+		clear();
     }
+	public void clear(){
+		for(int i=0; i<data.length;i++){
+			for(int j=0; j<data[i].length; j++){
+				data[i][j] = ' ';
+			}
+		}
+	}
+
+		
+
+	public static void main(String[]args){
+		WordGrid grid = new WordGrid(5,5);
+		System.out.println(grid);
+	} 
+
+
 }
