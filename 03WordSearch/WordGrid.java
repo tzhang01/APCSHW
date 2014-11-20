2860076be487e40d1ec6dfc9d98492f3a4037704
tzhang01 <tinazhang1197@gmail.com>
2014-11-20 00:51:50 -0500

    added addWordHorizontal method; works, but still have to fix the indexOutOfBoundsException

--- 03WordSearch/WordGrid.java
+++ 03WordSearch/WordGrid.java
@@ -41,10 +41,39 @@ public class WordGrid{
 		}
 		return result;
 	}
+
+	 /**Attempts to add a given word to the specified position of the WordGrid.
+     *The word is added from left to right, must fit on the WordGrid, and must
+     *have a corresponding letter to match any letters that it overlaps.
+     *
+     *@param word is any text to be added to the word grid.
+     *@param row is the vertical locaiton of where you want the word to start.
+     *@param col is the horizontal location of where you want the word to start.
+     *@return true when the word is added successfully. When the word doesn't fit,
+     *or there are overlapping letters that do not match, then false is returned.
+     */
+    public boolean addWordHorizontal(String word,int row, int col){
+		int column = col;		
+		for(int i=0; i<word.length();i++){
+			if(word.charAt(i)== data[row][column]|| data[row][column] != ' '){
+				return false;
+			}
+			column++;
+		}
+		column = col;
+		for(int i=0;i<word.length();i++){
+			data[row][column] = word.charAt(i);
+			column++;
+		}
+		return true;
+	}
 		
 	//testing
 	public static void main(String[]args){
 		WordGrid data = new WordGrid(6,6);
 		System.out.println("Should print out a grid of spaces" + data);
+		System.out.println("Should print True: " + data.addWordHorizontal("cat",1,1));
+		System.out.println(data);
+		//System.out.println("Should print False: " + data.addWordHorizontal("cat",4,4));
 	}
 }
