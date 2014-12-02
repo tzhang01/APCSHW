6b639b40609f02fe90db1f65e95dfd8381302554
Tina Zhang <tinazhang1197@gmail.com>
2014-12-02 13:15:35 -0500

    added add methods

--- 04OrderedSuperArray/OrderedSuperArray.java
+++ 04OrderedSuperArray/OrderedSuperArray.java
@@ -1,5 +1,20 @@
-public class OrderedSuperArray extend SuperArray{
-    public void add(int index, String 0){
+public class OrderedSuperArray extends SuperArray{
+    public void add(int index, String o){
+	add(o);
+    }
+    public void add(String o){
+	if(size() == 0){
+	    super.add(o);
+	}else{
+	    for(int i=0;i<size();i++){
+		if(get(i).compareTo(o) > 0 && get(i+1).compareTo(o) <0){
+		    super.add(i,o);
+		}
+	    }
+	}
+    }
+
+}
 	
     
     
